696f94e19b874d2ffcaf628d0133103b12cc9f07
Yolanda Robla <yroblamo@redhat.com>
2019-01-31 13:27:12 +0100

    Add fixes in file creation

--- third-party/installer/modify_bootstrap.sh
+++ third-party/installer/modify_bootstrap.sh
@@ -30,7 +30,7 @@ function add_ignition_file() {
     "source": "data:text/plain;charset=utf-8;base64,${content}",
     "verification": {}
   },
-  "mode": "${perm}"
+  "mode": ${perm}
 }
 EOF
     IGNITION_CONTENT=$(echo "${IGNITION_CONTENT}" | jq -c " . [\"storage\"][\"files\"] += [$file_entry]")
@@ -38,7 +38,12 @@ EOF
 
 IGNITION_CONTENT=$(cat $1)
 ETCD_CONTENTS="[Unit]\nDescription=Start etcd pod\nBefore=bootkube.service\nConditionPathExists=!/opt/openshift/.bootetcd.done\n\n[Service]\nWorkingDirectory=/opt/openshift\nExecStart=/usr/local/bin/bootetcd.sh\n\nRestart=on-failure\nRestartSec=5s\n"
-PODETCD_CONTENTS="podman run -d  -p 2379:2379   -p 2380:2380 --net=host  --name etcd quay.io/coreos/etcd:latest   /usr/local/bin/etcd   --data-dir=/etcd-data --name node1   --initial-advertise-peer-urls http://127.0.0.1:2380 --listen-peer-urls http://127.0.0.1:2380   --advertise-client-urls http://127.0.0.1:2379 --listen-client-urls http://127.0.0.1:2379   --initial-cluster node1=http://127.0.0.1:2380"
+
+read -r -d '' PODETCD_CONTENTS <<EOF
+#!/bin/bash
+podman run -d  -p 2379:2379   -p 2380:2380 --net=host  --name etcd quay.io/coreos/etcd:latest   /usr/local/bin/etcd   --data-dir=/etcd-data --name node1   --initial-advertise-peer-urls http://127.0.0.1:2380 --listen-peer-urls http://127.0.0.1:2380   --advertise-client-urls http://127.0.0.1:2379 --listen-client-urls http://127.0.0.1:2379   --initial-cluster node1=http://127.0.0.1:2380
+touch /opt/openshift/.bootetcd.done
+EOF
 
 add_ignition_unit "bootetcd.service" "${ETCD_CONTENTS}"
 add_ignition_file "/usr/local/bin/bootetcd.sh" root 365 root "${PODETCD_CONTENTS}"
